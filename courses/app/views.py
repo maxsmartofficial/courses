a8abac59f9c4070e7d9515b9edb53b4c4bdfdaa9
maxsmartofficial <msmart4156@gmail.com>
2021-03-27 15:32:23 +0000

    Add course page

--- courses/app/views.py
+++ courses/app/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Course
+
 # Create your views here.
 
 def index(request):
-	return(HttpResponse("Hello!"))
+	# All available courses
+	courses = Course.objects.all()
+	
+	return(render(request, 'app/index.html', {"courses": courses}))
+	
+def view_course(request, course_slug):
+	# View details about a course
+	c = Course.objects.get(slug=course_slug)
+	
+	return(render(request, 'app/course.html', {"course": c}))
+	
+	
