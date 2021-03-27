b048882cb16db9535567649f7da6a39271ee56b7
maxsmartofficial <msmart4156@gmail.com>
2021-03-27 17:07:32 +0000

    Add short description

--- courses/app/models.py
+++ courses/app/models.py
@@ -5,12 +5,14 @@ class Course(models.Model):
 	
 	name = models.CharField(max_length=200)
 	description = models.TextField(max_length=4000)
+	short_description = models.TextField(max_length=200, null=True)
 	slug = models.SlugField(max_length=200, null=True)
 	
 class Module(models.Model):
 	
 	name = models.CharField(max_length=200)
 	description = models.TextField(max_length=4000)
+	short_description = models.TextField(max_length=200, null=True)
 	order = models.IntegerField(default=0)
 	course = models.ForeignKey(Course, on_delete=models.CASCADE)
 	
