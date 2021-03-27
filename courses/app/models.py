68c5ab6b6f455a4c141ccfcc8da015b62498d887
maxsmartofficial <msmart4156@gmail.com>
2021-03-27 14:26:16 +0000

    Basic course structure

--- courses/app/models.py
+++ courses/app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+	
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=4000)
+	
+class Module(models.Model):
+	
+	name = models.CharField(max_length=200)
+	description = models.CharField(max_length=4000)
+	order = models.IntegerField(default=0)
+	course = models.ForeignKey(Course, on_delete=models.CASCADE)
+	
+class Research(models.Model):
+
+	module = models.ForeignKey(Module, on_delete=models.CASCADE)
+	text = models.CharField(max_length=40000)
+	
+class Assignment(models.Model):
+	module = models.ForeignKey(Module, on_delete=models.CASCADE)
+	text = models.CharField(max_length=40000)
+	
