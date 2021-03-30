cf3ac18ea1f943d4f0220f80cbb490e857c45712
maxsmartofficial <msmart4156@gmail.com>
2021-03-30 16:57:00 +0100

    Allow user to see next available module and complete courses

--- courses/app/models.py
+++ courses/app/models.py
@@ -65,6 +65,21 @@ class CourseInstanceManager(models.Manager):
 			module_instance.save()
 			
 		return(course_instance)
+		
+	def getModules(self, course_instance): # For user
+		"""Get ordered modules for a course_instance"""
+		return(ModuleInstance.objects.filter(course_instance=course_instance).order_by('module__order'))
+		
+		
+	def previousModuleCompleted(self, module_instance): # For user
+		"""Return True if the previous module is completed, or if it's the first module"""
+		course_instance = module_instance.course_instance
+		module_list = list(self.getModules(course_instance))
+		index = module_list.index(module_instance)
+		if index == 0:
+			return(True)
+		else:
+			return(module_list[index - 1].is_completed())
 
 
 class CourseInstance(models.Model):
@@ -74,6 +89,15 @@ class CourseInstance(models.Model):
 	startdate = models.DateTimeField(null=True)
 	
 	objects = CourseInstanceManager()
+	
+	def is_completed(self): # For user
+		# Return True if all modules are completed
+		modules = CourseInstance.objects.getModules(self)
+		all_complete = True
+		for m in modules:
+			if not m.is_completed():
+				all_complete = False
+		return(all_complete)
 
 
 
@@ -87,8 +111,22 @@ class ModuleInstance(models.Model):
 	completed = models.BooleanField(default=False)
 	
 	def is_completed(self):
+		# Completed if there is a module review
 		return(hasattr(self, 'modulereview'))
+		
+	def is_available(self):
+		# Available if it is past the startdate or the previous module is completed
+		if CourseInstance.objects.previousModuleCompleted(self):
+			return(True)
+		else:
+			now = datetime.datetime.now().astimezone()
+			return(now > self.startdate.astimezone())
+		
 
+	def is_past_deadline(self):
+		# Return True if it is past the course deadline
+		now = datetime.datetime.now().astimezone()
+		return(now > self.deadline.astimezone())
 	
 	
 class ModuleReview(models.Model):
